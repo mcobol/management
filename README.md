d9d226e85257979bbb3f3ae8ecc0835a205c06db
mcobol <mcobol@naver.com>
2022-04-04 19:23:40 +0900

    notebook error-code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## 본 프로젝트는 학습용으로 만들어진 고객 관리 시스템입니다.
+## 본 프로젝트는 React 학습용으로 만들어진 고객 관리 시스템입니다.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [{
+  'id':1,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'오치남',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'이순신',
+  'birthday':'961333',
+  'gender':'남자',
+  'job':'군인'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/any',
+  'name':'홍길동',
+  'birthday':'961444',
+  'gender':'남자',
+  'job':'프로그래머'
+}];
 
 class App extends Component {
   render() {
-    return (
-      <div className="gray-background">
-         <img src={logo} lat="logo" />
-         <h2>Let's develop management system!</h2>
-      </div>
-    );
+    return ( 
+      < >
+        {
+          customers.map(c => {
+            return (
+              <HTMLDivElement>
+                {
+                <Customer
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+          }
+              </HTMLDivElement>
+            )
+          })
+        }
+      </>  
+    )
   }
 }
+
 
 export default App;
